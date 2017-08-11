c3ec97505e17ae9540e20fa2da63b2e2d8a74f7b
eamanu <emmanuelarias30@gmail.com>
2017-08-11 17:01:01 -0300

    new changes

--- src/tit.py
+++ src/tit.py
@@ -14,30 +14,56 @@ print(train.isnull().sum())
 
 sobrev = train[train['Survived'] == 1]
 nosobrev = train[train['Survived'] == 0]
-s_percent = s/total*100.0
 
 def porcentajeSobreviviente(sobrev, nosobrev):
-    total = sobrev + nosobrev
+    total = len(sobrev) + len(nosobrev)
     s = float(len(sobrev))
     ns = float(len(nosobrev))
-    print('Sobrevivió el %f porciento. En el DataFrame hay un total de %i sobrevivientes y un\
+    s_percent = s/total*100.0
+    print('Sobrevivió el %.3f porciento. En el DataFrame hay un total de %i sobrevivientes y un\
 total de %i no sobrevivientes' %(s_percent, s, ns))
 
 # Print % sobrevivientes
-porcentajeSobreviviente(sobrev, nosobrev)
+# porcentajeSobreviviente(sobrev, nosobrev)
 
 sobre_color = 'green'
 nosobre_color = 'red'
 
 
-plt.figure()
-# Age study
-sns.distplot(sobrev['Age'].dropna().values, bins=range(0, 100, 1), color=sobre_color, label='Sobrevivio')
-sns.distplot(nosobrev['Age'].dropna().values, bins=range(0, 100, 1), color=nosobre_color,  axlabel='Age', label="No sobrevivio")
-plt.show()
-# Sex study
-plt.figure()
-sns.barplot('Sex', 'Survived', data=train, palette='Reds_d')
-plt.show()
+def drawFirstVis(sobrev, nosobrev, sobre_color, nosobre_color):
+    plt.figure()
+    # Age study
+    sns.distplot(sobrev['Age'].dropna().values, bins=range(0, 100, 1), color=sobre_color, label='Sobrevivio')
+    sns.distplot(nosobrev['Age'].dropna().values, bins=range(0, 100, 1), color=nosobre_color,  axlabel='Age', label="No sobrevivio")
+    plt.show()
+    # Sex study
+    plt.figure()
+    sns.barplot('Sex', 'Survived', data=train, palette='Reds_d')
+    plt.show()
+    # Pclass Study
+    plt.figure()
+    sns.barplot('Pclass', 'Survived', data=train)
+    plt.show()
+    # SibSp study
+    plt.figure()
+    sns.barplot('SibSp', 'Survived', data=train)
+    plt.show()
+    # Parch study
+    plt.figure()
+    sns.barplot('Parch', 'Survived', data=train)
+    plt.show()
+    # Embarked Study
+    plt.figure()
+    sns.barplot('Embarked', 'Survived', data=train)
+    plt.show()
+    # Fare study
+    plt.figure()
+    sns.distplot(sobrev['Fare'].dropna().values, bins=range(0, 513, 1), color=sobre_color, label='Sobrevivio')
+    sns.distplot(nosobrev['Fare'].dropna().values, bins=range(0, 513, 1), color=nosobre_color, label='No Sobrevivio', axlabel="Fare")
+    plt.show()
 
+# drawFirstVis(sobrev, nosobrev, sobre_color, nosobre_color)
+
+tab = pd.crosstab(train['Parch'], train['Survived'])
+print(tab)
 
